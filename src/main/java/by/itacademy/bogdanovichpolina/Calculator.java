40a4e8a9fa6a30b55e0db6a97062de174fd250ea
Polina Bogdanovich <yapolli10@gmail.com>
2025-07-30 21:42:43 +0300

    add class Calculator and interface ICalculator

--- src/main/java/by/itacademy/bogdanovichpolina/Calculator.java
+++ src/main/java/by/itacademy/bogdanovichpolina/Calculator.java
@@ -1,4 +1,18 @@
 package by.itacademy.bogdanovichpolina;
 
-public class Calculator {
+public class Calculator implements ICalculator {
+    @Override
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int multiply(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public double divide(int a, int b) {
+        return (double) a / b;
+    }
 }
---
+++ src/main/java/by/itacademy/bogdanovichpolina/ICalculator.java
@@ -0,0 +1,7 @@
+package by.itacademy.bogdanovichpolina;
+
+public interface ICalculator {
+    int sum(int a, int b);
+    int multiply(int a, int b);
+    double divide(int a, int b);
+}
