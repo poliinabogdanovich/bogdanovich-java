ad21922bb28fe4924feefcf7a7ef86a680eac193
Polina Bogdanovich <yapolli10@gmail.com>
2025-08-04 00:38:05 +0300

    add 4 methods for Calculator

--- src/main/java/by/itacademy/bogdanovichpolina/Calculator.java
+++ src/main/java/by/itacademy/bogdanovichpolina/Calculator.java
@@ -2,17 +2,37 @@ package by.itacademy.bogdanovichpolina;
 
 public class Calculator implements ICalculator {
     @Override
-    public int sum(int a, int b) {
-        return a + b;
+    public int sum(int a, int b) throws ArithmeticException {
+        try {
+            return Math.addExact(a, b);
+        } catch (Exception e) {
+            throw new ArithmeticException("Выход за границы диапазона значений" + e.getMessage());
+        }
+    }
+
+    @Override
+    public int subtract(int a, int b) throws ArithmeticException {
+        try {
+            return Math.subtractExact(a, b);
+        } catch (Exception e) {
+            throw new ArithmeticException("Выход за границы диапазона значений" + e.getMessage());
+        }
     }
 
     @Override
     public int multiply(int a, int b) {
-        return a - b;
+        try {
+            return Math.multiplyExact(a, b);
+        } catch (Exception e) {
+            throw new ArithmeticException("Выход за границы диапазона значений" + e.getMessage());
+        }
     }
 
     @Override
     public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Ошибка деления на ноль");
+        }
         return (double) a / b;
     }
 }
