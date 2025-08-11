06137b3ba1b0735afcbfce1cff0db945e2a7ccf5
Polina Bogdanovich <yapolli10@gmail.com>
2025-08-11 03:00:59 +0300

    refactor code

--- src/main/java/by/dodopizza/HomePageLocator.java
+++ src/main/java/by/dodopizza/HomePageLocator.java
@@ -2,6 +2,6 @@ package by.dodopizza;
 
 public class HomePageLocator {
     public static final String BUTTON_HEADER_LOGIN = "//button[@data-testid='header_login']";
-    public static final String BUTTON_NAVIGATION_CART = "//button[@data-testid=\"navigation__cart\"]";
+    public static final String BUTTON_NAVIGATION_CART = "//button[@data-testid='navigation__cart']";
 
 }
